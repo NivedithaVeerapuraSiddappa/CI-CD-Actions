9c073c8f97673c6e0632edad5d0383fa0d423403
VSN9KOR <vsniveditha08@gmail.com>
2025-10-29 11:39:55 +0530

    Add Ecommerce.Web project

--- Ecommerce.Web/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace Ecommerce.Web.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
